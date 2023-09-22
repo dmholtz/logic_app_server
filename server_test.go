31566a1bbf2587311f89e6817a03e9a70be588c9
dmholtz <56723830+dmholtz@users.noreply.github.com>
2023-09-22 10:54:31 +0200

    Add tests for QuizHandler

--- server_test.go
+++ server_test.go
@@ -126,7 +126,7 @@ func TestPlayersHandler(t *testing.T) {
 
 	t.Run("GET /players/player/achievement returns 200 if not authorized", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/players/player/achievements", nil)
-		//request.Header.Set("Authorization", "Bearer unknown")
+		request.Header.Set("Authorization", "Bearer unknown")
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
@@ -134,4 +134,56 @@ func TestPlayersHandler(t *testing.T) {
 		assert.Equal(t, http.StatusUnauthorized, response.Code)
 	})
 
+}
+
+func TestQuizHandler(t *testing.T) {
+	tempfile, err := os.CreateTemp("", "test_db.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tempfile.Name())
+
+	dbBytes, err := os.ReadFile("./db_test.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tempfile.Write(dbBytes)
+
+	db, dbErr := sql.Open("sqlite3", tempfile.Name())
+	if dbErr != nil {
+		log.Fatal(dbErr)
+	}
+	defer db.Close()
+
+	server := NewLogicAppServer(db)
+
+	t.Run("GET /quiz/competition returns 200", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/quiz/competition", nil)
+		request.Header.Set("Authorization", "Bearer 2TeSZoUxu3a1qCc/23KZ8PCKgeABYGuFpRgEWMw6skQ=")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("GET /quiz/find returns 200", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/quiz/find?type=TAUT&difficulty=easy&numVars=2&timeLimit=1000", nil)
+		request.Header.Set("Authorization", "Bearer 2TeSZoUxu3a1qCc/23KZ8PCKgeABYGuFpRgEWMw6skQ=")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("GET /quiz/find returns 200", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/quiz/find?type=SAT&difficulty=easy&numVars=5&timeLimit=1000", nil)
+		request.Header.Set("Authorization", "Bearer 2TeSZoUxu3a1qCc/23KZ8PCKgeABYGuFpRgEWMw6skQ=")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
 }
